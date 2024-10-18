5221ca8f6361d043b0407e22c615228c1f607842
JohnTheCoolingFan <ivan8215145640@gmail.com>
2024-10-18 18:14:15 +0300

    Initial commit

---
+++ edit_details.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -X POST -d mod="${MOD_NAME}" -d description="${DESCRIPTION_FILE}" -H "Authorization: Bearer ${FACTORIO_PORTAL_TOKEN}"  https://mods.factorio.com/api/v2/mods/edit_details
